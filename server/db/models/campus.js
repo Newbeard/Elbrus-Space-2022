797abe5ca2671f4481c0f96eac4dc5ec462db9a2
Alex <alekscherniaev@gmail.com>
2022-03-17 18:36:06 +0300

    change model

--- server/db/models/campus.js
+++ server/db/models/campus.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate({ User }) {
-      Campus.belongsToMany(User, { foreignKey: 'campusId' });
+      Campus.hasMany(User, { foreignKey: 'campusId' });
     }
   }
   Campus.init({
