a816e9ed7716dbfe177320e153f334a8a2f616c2
virekin <virekintoo@bk.ru>
2022-03-18 12:33:36 +0300

    add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import Nav from './components/Nav/Nav';
 import Registration from './components/Registration/Registration';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
-// import { useEffect } from 'react';
 // import { useDispatch } from 'react-redux';
 // import { sessionCheck } from './redux/actions/regLogOut.actions';
 // import axios from 'axios';
@@ -13,34 +12,36 @@ import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import FindPeople from './components/FindPeople/FindPeople';
 import EditProfile from './components/EditProfile/EditProfile';
-
-import Maps from './components/Map/Map';
-import Diagram from './components/Diagram/Diagram';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [isMobile, setIsMobile] = useState(true)
+  const screenWidth = window.innerWidth
   
+  useEffect(() => {
+    screenWidth < 768 ? setIsMobile(true) : setIsMobile(false)
+  }, [])
+
 	return (
 		<>
 			<div className="wraper">
 				<header className="header">
-					<Nav />
+
+          {!isMobile && (<Nav />)}
+					
 				</header>
-
-            {/* <FindPeople /> */}
-            <EditProfile />
-
-
-        <Maps/>
-        <Diagram/>
-       
-              
+    
 				<main className="uk-container">
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/registration" element={<Registration />} />
 						<Route path="/login" element={<Login />} />
+						<Route path="/search" element={<FindPeople />} />
+						<Route path="/profile" element={<EditProfile />} />
 					</Routes>
 				</main>
+
         <footer>
           <Footer />
         </footer>
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,9 +1,19 @@
+import { Link } from 'react-router-dom';
+import styles from './style.module.css'
 
 export default function Nav() {
 
   return (
     <div>
-
+      {/* <Link /> */}
+      <nav id="nav">
+        <ul className="container">
+          <li><Link to="/">Главная</Link></li>
+          <li><Link to="/search">Поиск</Link></li>
+          <li><Link to="/profile">Мой профиль</Link></li>
+          <li><Link to="/logout">Выйти</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 }
