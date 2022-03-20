675fc4fde823a5a549b647e621b33fb4d56879b3
virekin <virekintoo@bk.ru>
2022-03-20 15:05:41 +0300

    add create newCountries add newCities

--- server/controllers/addInfoController.js
+++ server/controllers/addInfoController.js
@@ -1,11 +1,37 @@
-const { User } = require('../db/models');
+const { User, Country, City } = require('../db/models');
 
 const addInfoController = async (req, res) => {
+  const { dataForm, id } = req.body;
+  const { name, surName, countryName, cityName } = dataForm;
   try {
-    const info = req.body;
-    console.log(info);
-    const students = await User.update();
-    res.json(students);
+    const country = await Country.findOne({ where: { countryName } });
+    const city = await City.findOne({ where: { cityName } });
+    if (!country) {
+      const newCountry = await Country.create({ countryName });
+      const newCity = await City.create({ cityName, countrysId: newCountry.id });
+      await User.update(
+        {
+          name,
+          surName,
+          countryId: newCountry.id,
+          cityId: newCity.id,
+        },
+        { where: { id } },
+      );
+    } else if (!city) {
+      const newCity = await City.create({ cityName, countrysId: country.id });
+      await User.update(
+        {
+          name,
+          surName,
+          countryId: country.id,
+          cityId: newCity.id,
+        },
+        { where: { id } },
+      );
+    }
+    // await User.update(dataForm, { where: { id } });
+    // res.json(students);
   } catch (error) {
     res.sendStatus(500);
   }
