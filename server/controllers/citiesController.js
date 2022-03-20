5768f69b635c96a33e3d790d7f91a58cfdd97d49
Denis <chepelnikov.denis@yandex.ru>
2022-03-20 16:56:30 +0300

    add to merge

--- server/controllers/citiesController.js
+++ server/controllers/citiesController.js
@@ -1,9 +1,6 @@
-
 const axios = require('axios');
 
-
 const { City, Country } = require('../db/models');
-
 
 const citiesController = async (req, res) => {
   try {
@@ -18,26 +15,25 @@ const citiesController = async (req, res) => {
   }
 };
 
+// const getCoordinates = async (req, res) => {
+//   try {
+//     const allCities = await City.findAll({ raw: true, attributes: ['cityName'] });
+//     console.log(allCities);
 
-const getCoordinates = async (req, res) => {
-  try {
-    const allCities = await City.findAll({ raw: true, attributes: ['cityName'] });
-    console.log(allCities);
+//     const arrCity = allCities.map(((el) => (el.cityName)));
+//     console.log(arrCity);
+//     arrCity.forEach((element) => {
+//       const getCoordinate = axios.get(`https://geocode-maps.yandex.ru/1.x/?format=json&apikey=fa906837-e249-4c18-99ac-fb6aff0bc767&geocode=${encodeURIComponent(element)}`)
+//         .then((response) => console.log(response.data.response.GeoObjectCollection.featureMember));
 
-    const arrCity = allCities.map(((el) => (el.cityName)));
-    console.log(arrCity);
-    arrCity.forEach((element) => {
-      const getCoordinate = axios.get(`https://geocode-maps.yandex.ru/1.x/?format=json&apikey=fa906837-e249-4c18-99ac-fb6aff0bc767&geocode=${encodeURIComponent(element)}`)
-        .then((response) => console.log(response.data.response.GeoObjectCollection.featureMember));
+//       console.log(getCoordinate);
+//     });
+//     const [arrGeo] = getCoordinate.data.response.GeoObjectCollection.featureMember;
+//     // res.json(getCoordinate);
+//     console.log(arrGeo.GeoObject.Point.pos);
+//     res.json(getCoordinate.data.response.GeoObjectCollection);
 
-      console.log(getCoordinate);
-    });
-    const [arrGeo] = getCoordinate.data.response.GeoObjectCollection.featureMember;
-    // res.json(getCoordinate);
-    console.log(arrGeo.GeoObject.Point.pos);
-    res.json(getCoordinate.data.response.GeoObjectCollection);
-
-    // console.log(allCities);
+//     // console.log(allCities);
 
 const citiesOfSelectedCountryController = async (req, res) => {
   try {
@@ -50,7 +46,6 @@ const citiesOfSelectedCountryController = async (req, res) => {
       raw: true,
     });
     res.json(allCities);
-
   } catch (error) {
     console.log(error.message);
     res.status(401)
@@ -60,8 +55,4 @@ const citiesOfSelectedCountryController = async (req, res) => {
   }
 };
 
-module.exports = { citiesController, getCoordinates, citiesOfSelectedCountryController};
-=======
-
-
-
+module.exports = { citiesController, citiesOfSelectedCountryController };
