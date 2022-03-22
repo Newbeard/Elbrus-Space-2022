fe6c3201ed10d47916d6a41401d214939c8d4384
Alfia <alfia.ah7@gmail.com>
2022-03-22 22:10:23 +0300

    skeleton of diagram

--- client/src/components/Diagram/Diagram.jsx
+++ client/src/components/Diagram/Diagram.jsx
@@ -1,102 +1,123 @@
 import React from 'react';
-import {
-  // main component
-  Chart,
-  // graphs
-  Bars, Cloud, Dots, Labels, Lines, Pies, RadialLines, Ticks, Title,
-  // wrappers
-  Layer, Animate, Transform, Handlers,
-  // helpers
-  helpers, DropShadow, Gradient
-} from 'rumble-charts';
+import "@progress/kendo-theme-material/dist/all.css";
+import "hammerjs";
+import "./Diagram.css";
+import Donut from "./Donut";
 
 function Diagram(props) {
-  const series = [{
-  data: [1, 2, 3]
-}, {
-  data: [5, 7, 11]
-}, {
-  data: [13, 17, 19]
-}];
   return (
-    <div>
-      Hello from diagram
-      <Chart
-  height={300}
-  minY={0}
-  series={[
-    {
-      data: [
-        1,
-        2,
-        3
-      ],
-      name: 'Moscow'
-    },
-    {
-      data: [
-        5,
-        7,
-        11
-      ],
-      name: 'Saint-Petersburg'
-    },
-    {
-      data: [
-        13,
-        17,
-        19
-      ],
-      name: 'Kazan'
-    }
-  ]}
-  style={{
-    fontFamily: 'sans-serif',
-    fontSize: '0.75em'
-  }}
-  width={600}
->
-  <Layer
-    height="90%"
-    position="top center"
-    width="80%"
-  >
-    <Ticks
-      axis="y"
-      labelAttributes={{
-        x: -5
-      }}
-      labelStyle={{
-        dominantBaseline: 'middle',
-        fill: 'lightgray',
-        textAnchor: 'end'
-      }}
-      lineLength="100%"
-      lineStyle={{
-        stroke: 'lightgray'
-      }}
-      lineVisible
-    />
-    <Ticks
-      axis="x"
-      label={function noRefCheck(){}}
-      labelAttributes={{
-        y: 3
-      }}
-      labelStyle={{
-        dominantBaseline: 'text-before-edge',
-        fill: 'lightgray',
-        textAnchor: 'middle'
-      }}
-    />
-    <Bars
-      groupPadding="3%"
-      innerPadding="0.5%"
-    />
-  </Layer>
-</Chart>
+    <div className="App">
+      <div className="container">
+        <h1>Статистика</h1>
+        <div className="section">
+          <Donut />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Diagram;
+
+// import React from 'react';
+// import {
+//   // main component
+//   Chart,
+//   // graphs
+//   Bars, Cloud, Dots, Labels, Lines, Pies, RadialLines, Ticks, Title,
+//   // wrappers
+//   Layer, Animate, Transform, Handlers,
+//   // helpers
+//   helpers, DropShadow, Gradient
+// } from 'rumble-charts';
+
+// function Diagram(props) {
+//   const series = [{
+//   data: [1, 2, 3]
+// }, {
+//   data: [5, 7, 11]
+// }, {
+//   data: [13, 17, 19]
+// }];
+//   return (
+//     <div>
+//       Hello from diagram
+//       <Chart
+//   height={300}
+//   minY={0}
+//   series={[
+//     {
+//       data: [
+//         1,
+//         2,
+//         3
+//       ],
+//       name: 'Moscow'
+//     },
+//     {
+//       data: [
+//         5,
+//         7,
+//         11
+//       ],
+//       name: 'Saint-Petersburg'
+//     },
+//     {
+//       data: [
+//         13,
+//         17,
+//         19
+//       ],
+//       name: 'Kazan'
+//     }
+//   ]}
+//   style={{
+//     fontFamily: 'sans-serif',
+//     fontSize: '0.75em'
+//   }}
+//   width={600}
+// >
+//   <Layer
+//     height="90%"
+//     position="top center"
+//     width="80%"
+//   >
+//     <Ticks
+//       axis="y"
+//       labelAttributes={{
+//         x: -5
+//       }}
+//       labelStyle={{
+//         dominantBaseline: 'middle',
+//         fill: 'lightgray',
+//         textAnchor: 'end'
+//       }}
+//       lineLength="100%"
+//       lineStyle={{
+//         stroke: 'lightgray'
+//       }}
+//       lineVisible
+//     />
+//     <Ticks
+//       axis="x"
+//       label={function noRefCheck(){}}
+//       labelAttributes={{
+//         y: 3
+//       }}
+//       labelStyle={{
+//         dominantBaseline: 'text-before-edge',
+//         fill: 'lightgray',
+//         textAnchor: 'middle'
+//       }}
+//     />
+//     <Bars
+//       groupPadding="3%"
+//       innerPadding="0.5%"
+//     />
+//   </Layer>
+// </Chart>
+//     </div>
+//   );
+// }
+
+// export default Diagram;
---
+++ client/src/components/Diagram/constants.js
@@ -0,0 +1,6 @@
+export const COLORS = {
+  moscow: "#059669",
+  online: "#d48300",
+  spb: "#7e00a5",
+  total: "#D97706",
+};
