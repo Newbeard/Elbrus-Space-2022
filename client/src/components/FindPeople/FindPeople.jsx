1ba4a8b1bab0160d66d2e075cf460bfc58a929d4
virekin <virekintoo@bk.ru>
2022-03-24 02:26:53 +0300

    student list

--- client/src/components/FindPeople/FindPeople.jsx
+++ client/src/components/FindPeople/FindPeople.jsx
@@ -34,19 +34,31 @@ function FindPeople() {
   return (
     <div className='container-find'>
       <div className='container-search'>
-      <button onClick={showFilter}>Фильтр</button>
-      <div className='input-with-icon-box-find'>
-        <input className='input-search' type="text" name="name" id="name" placeholder="Поиск" />
-        <svg onClick={showFilter} width="25" height="25" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M13.5935 23.4375C19.0301 23.4375 23.4373 19.0303 23.4373 13.5938C23.4373 8.1572 19.0301 3.75 13.5935 3.75C8.15695 3.75 3.74976 8.1572 3.74976 13.5938C3.74976 19.0303 8.15695 23.4375 13.5935 23.4375Z" stroke="#D1D1D1" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-          <path d="M20.5537 20.5547L26.2491 26.2501" stroke="#D1D1D1" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
+        <button onClick={showFilter}>Фильтр</button>
+        <div className='input-with-icon-box-find'>
+          <input className='input-search' type="text" name="name" id="name" placeholder="Поиск" />
+          <svg onClick={showFilter} width="25" height="25" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.5935 23.4375C19.0301 23.4375 23.4373 19.0303 23.4373 13.5938C23.4373 8.1572 19.0301 3.75 13.5935 3.75C8.15695 3.75 3.74976 8.1572 3.74976 13.5938C3.74976 19.0303 8.15695 23.4375 13.5935 23.4375Z" stroke="#D1D1D1" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M20.5537 20.5547L26.2491 26.2501" stroke="#D1D1D1" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </div>
+        {isShowFilter && <FilterModal city={city} countries={countries} closedFilterStudents={closedFilterStudents} />}
+        <div className='cards-students'>
+          {students && students.map((student) =>
+            <div className='card-student' key={student.id}>
+              <div className='foto-student'>{student.name.split('')[0]}</div>
+              <Link to={`/student/${student.id}`} className="info-student" style={{ textDecoration: 'none' }}>
+                <div className='name-student'>
+                  {student.name} {student.surName}
+                </div>
+                <div className='student-city'>
+                  {student.surName}
+                </div>
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
-      {isShowFilter && <FilterModal city={city} countries={countries} closedFilterStudents={closedFilterStudents} />}
-      <div className='cards-students'>
-        {students && students.map((student) => <div className='card-student' key={student.id}><Link to={`/student/${student.id}`} style={{ textDecoration: 'none' }}>{student.name} {student.surName}</Link></div>)}
-      </div>
-    </div>
     </div>
   );
 }
