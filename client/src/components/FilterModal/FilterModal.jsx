8e04db134ba93ec3756027d554a4a0d1c184ce2f
virekin <virekintoo@bk.ru>
2022-03-23 20:56:59 +0300

    refac component filter

---
+++ client/src/components/FilterModal/FilterModal.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getCitiesOfSelectedCountryFromServer } from '../../redux/actions/city.action';
+import { filterStudentsFromServer } from '../../redux/actions/students.action';
+
+function FilterModal({ city, countries, resetFilterStudents }) {
+  const dispatch = useDispatch();
+  const [countrySelected, setCountrySelected] = useState('');
+
+  function selectedСountry(event) {
+    setCountrySelected(event.target.value);
+    dispatch(getCitiesOfSelectedCountryFromServer({ countryName: event.target.value }))
+  }
+
+  function filterStudents(event) {
+    event.preventDefault()
+    const form = event.target;
+    const dataForm = Object.fromEntries(new FormData(form));
+    dispatch(filterStudentsFromServer(dataForm))
+  }
+
+  return (
+    <div>
+      <div className='search-form'>
+          <form method="post" onSubmit={filterStudents} action="#">
+            <select onChange={(event) => selectedСountry(event)} name="countryName" >
+              <option value="" disabled selected>Страна</option>
+              <option>Любая</option>
+              {countries.map((country) => <option key={country.id}>{country.countryName}</option>)}
+            </select>
+            {countrySelected === "" ?
+              <select disabled name="cityName" >
+                <option value="" disabled selected>Город</option>
+                <option>Любая</option>
+                {city?.map((city) => <option key={city.id}>{city.cityName}</option>)}
+              </select> :
+              <select name="cityName" >
+                <option value="" disabled selected>Город</option>
+                <option>Любая</option>
+                {city?.map((city) => <option key={city.id}>{city.cityName}</option>)}
+              </select>}
+            <select name="campusName" >
+              <option value="" disabled selected>Кампус</option>
+              <option>Любой</option>
+              <option>Москва</option>
+              <option>Санкт-Петербург</option>
+              <option>Онлайн</option>
+            </select>
+            <select name="yearFinishDate" >
+              <option value="" disabled selected>Год окончания</option>
+              <option>За всё время</option>
+              <option>2022</option>
+              <option>2021</option>
+            </select>
+            <select name="monthFinishDate" >
+              <option value="" disabled selected>Месяц окончания</option>
+              <option>За всё время</option>
+              <option>Январь</option>
+              <option>Февраль</option>
+              <option>Март</option>
+              <option>Апрель</option>
+              <option>Май</option>
+              <option>Июнь</option>
+              <option>Июль</option>
+              <option>Август</option>
+              <option>Сентябрь</option>
+              <option>Октябрь</option>
+              <option>Ноябрь</option>
+              <option>Декабрь</option>
+            </select>
+            <input type="submit" value="Показать" />
+          </form>
+          <button onClick={resetFilterStudents}>Очистить фильтр</button>
+        </div>
+        : <div></div>
+    </div>
+  );
+}
+
+export default FilterModal;
--- client/src/components/FindPeople/FindPeople.jsx
+++ client/src/components/FindPeople/FindPeople.jsx
@@ -5,6 +5,7 @@ import { initCountriesFromServer } from '../../redux/actions/country.action';
 import { filterStudentsFromServer, initStudentsFromServer } from '../../redux/actions/students.action';
 import { Link } from 'react-router-dom';
 import './FindPeople.css'
+import FilterModal from '../FilterModal/FilterModal';
 
 function FindPeople() {
 
@@ -36,10 +37,7 @@ function FindPeople() {
     setCountrySelected('');
   }
 
-  function selectedСountry(event) {
-    setCountrySelected(event.target.value);
-    dispatch(getCitiesOfSelectedCountryFromServer({ countryName: event.target.value }))
-  }
+  
   return (
     <div className=''>
       <button onClick={showFilter}>Фильтр</button>
@@ -52,59 +50,7 @@ function FindPeople() {
           </svg>
         </button>
       </div>
-      {isShowFilter === true ?
-        <div className='search-form'>
-          <form method="post" onSubmit={filterStudents} action="#">
-            <select onChange={(event) => selectedСountry(event)} name="countryName" >
-              <option value="" disabled selected>Страна</option>
-              <option>Любая</option>
-              {countries.map((country) => <option key={country.id}>{country.countryName}</option>)}
-            </select>
-            {countrySelected === "" ?
-              <select disabled name="cityName" >
-                <option value="" disabled selected>Город</option>
-                <option>Любая</option>
-                {city?.map((city) => <option key={city.id}>{city.cityName}</option>)}
-              </select> :
-              <select name="cityName" >
-                <option value="" disabled selected>Город</option>
-                <option>Любая</option>
-                {city?.map((city) => <option key={city.id}>{city.cityName}</option>)}
-              </select>}
-            <select name="campusName" >
-              <option value="" disabled selected>Кампус</option>
-              <option>Любой</option>
-              <option>Москва</option>
-              <option>Санкт-Петербург</option>
-              <option>Онлайн</option>
-            </select>
-            <select name="yearFinishDate" >
-              <option value="" disabled selected>Год окончания</option>
-              <option>За всё время</option>
-              <option>2022</option>
-              <option>2021</option>
-            </select>
-            <select name="monthFinishDate" >
-              <option value="" disabled selected>Месяц окончания</option>
-              <option>За всё время</option>
-              <option>Январь</option>
-              <option>Февраль</option>
-              <option>Март</option>
-              <option>Апрель</option>
-              <option>Май</option>
-              <option>Июнь</option>
-              <option>Июль</option>
-              <option>Август</option>
-              <option>Сентябрь</option>
-              <option>Октябрь</option>
-              <option>Ноябрь</option>
-              <option>Декабрь</option>
-            </select>
-            <input type="submit" value="Показать" />
-          </form>
-          <button onClick={resetFilterStudents}>Очистить фильтр</button>
-        </div>
-        : <div></div>}
+      {isShowFilter === true ? <FilterModal city={city} countries={countries} resetFilterStudents={resetFilterStudents}  /> : <></>}
       <div className='cards-students'>
         {students && students.map((student) => <div className='card-student' key={student.id}><Link to={`/student/${student.id}`} style={{ textDecoration: 'none' }}>{student.name} {student.surName}</Link></div>)}
       </div>
