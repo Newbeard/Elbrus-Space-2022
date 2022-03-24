8708a917c23de8575c5137f294dfe37f06cf63e2
virekin <virekintoo@bk.ru>
2022-03-24 21:23:00 +0300

    fixed bags with diagrams

--- client/src/redux/reducers/students.reducer.js
+++ client/src/redux/reducers/students.reducer.js
@@ -14,9 +14,7 @@ export function studentsReducer(state = [], action) {
 
     case SEARCH_STUDENTS: {
       const regex = new RegExp(`^${payload}`, 'i')
-      // console.log(regex);
       const newState = state.filter((student) => regex.test(student.name))
-      // console.log(newState);
       return newState
     }
   
