bb313fa18e0a9f4123290c81b777662b96c991c5
virekin <virekintoo@bk.ru>
2022-03-25 13:23:50 +0300

    text error fixed

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -8,8 +8,8 @@ export default function Home(props) {
       <div className="container-map">
         <h1 className='header-map'>География студентов</h1>
         <Maps />
-        <div className='description-map'>На карте отмечены родные города студентов, поступивших на учебу в Эльбрус.</div>
-        <div className='description-map-bottom'>Так же можно увидеть количество студентов из одного города  </div>
+        <div className='description-map'>На карте отмечены родные города студентов, поступивших на учёбу в Эльбрус.</div>
+        <div className='description-map-bottom'>Также можно увидеть количество студентов из одного города.</div>
       </div>
     </div>
   );
--- client/src/components/NavbarAutorization/NavbarAuthorization.jsx
+++ client/src/components/NavbarAutorization/NavbarAuthorization.jsx
@@ -3,10 +3,9 @@ import { NavLink } from 'react-router-dom';
 import { useEffect, useState } from 'react'
 
 function NavbarAuthorization(props) {
-
   return (
     <div className='nav-login-registration'>
-      <NavLink to="/login" className='link-nav link'>
+      <NavLink to="/login"  className='link-nav link'>
           <h3 className="h3-link">Вход</h3>
       </NavLink>
       <NavLink to="/registration" className='link-nav link'>
