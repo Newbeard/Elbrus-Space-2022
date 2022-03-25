1a97e477205010c55a9942e697db7e770b7ba185
virekin <virekintoo@bk.ru>
2022-03-25 10:44:12 +0300

    layout diagram

--- client/src/components/Diagram/Diagram.jsx
+++ client/src/components/Diagram/Diagram.jsx
@@ -6,10 +6,12 @@ import './Diagram.css'
 
 function Diagram(props) {
   return (
-    <div className="container-diagram">
-      <h1>Статистика</h1>
-      <Chart />
-      <div>Cоотношение студентов, проходящих обучение в Москве, Санкт-Петербурге и Онлайн</div>
+    <div className="upper-container">
+      <div className="container-diagram">
+        <h1 className='header-diagram'>Статистика</h1>
+        <Chart />
+        <div className='description-diagram'>Cоотношение студентов, проходящих обучение в Москве, Санкт-Петербурге и Онлайн</div>
+      </div>
     </div>
   );
 }
