ca89978eb0fb7f562253533ec0329e6b03f02f9a
Alex <alekscherniaev@gmail.com>
2022-03-25 13:39:04 +0300

    add deploy

--- server/config/sessionConfig.js
+++ server/config/sessionConfig.js
@@ -6,7 +6,7 @@ const FileStore = sessionFileStore(session);
 const sessionConfig = {
   store: new FileStore(), // тип хранилища - FileStore, который создаёт нам папку с файлами
   key: 'user_id', // ключ - название куки
-  secret: `${process.env.SECRET_WORD}`, // слово используемое для шифрования, может быть любым
+  secret: `${process.env.SECRET_CLOVO}`, // слово используемое для шифрования, может быть любым
   resave: true, // настройка пересохранения куки, при каждом запросе
   saveUninitialized: false, // настройка для создания сессии, даже без авторизации
   cookie: {
